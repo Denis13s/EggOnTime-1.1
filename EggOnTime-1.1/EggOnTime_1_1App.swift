7116589f735ffe3e208413c18db7bef7d0f6df83
Denis <deni13yarec@gmail.com>
2023-09-01 13:38:50 +0200

    Initial Commit

---
+++ EggOnTime-1.1/EggOnTime_1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  EggOnTime_1_1App.swift
+//  EggOnTime-1.1
+//
+//  Created by Denis Yarets on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct EggOnTime_1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
